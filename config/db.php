1db5cfce7bf5e8126534bcd0f37fde8179d64815
hollylux <hollyluxl@gmail.com>
2017-03-28 17:51:52 -0400

    First commit

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=yiit',
+    'username' => 'root',
+    'password' => 'passw0rd',
+    'charset' => 'utf8',
+];
