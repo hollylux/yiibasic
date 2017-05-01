2894b4ffb1006381e80660475c5c6c6ed12af701
hollylux <hollyluxl@gmail.com>
2017-05-01 18:25:43 -0400

    Re-org order form

--- views/order/_form.php
+++ views/order/_form.php
@@ -11,30 +11,40 @@ use yii\widgets\ActiveForm;
 <div class="order-form">
 
     <?php $form = ActiveForm::begin(); ?>
-
-    <?= $form->field($model, 'product_id')->textInput() ?>
-
-    <?= $form->field($model, 'amount')->textInput() ?>
-
-    <?= $form->field($model, 'payment')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'user_id')->textInput() ?>
-
-    <?= $form->field($model, 'user_name')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'created_at')->textInput() ?>
-
-    <?= $form->field($model, 'updated_at')->textInput() ?>
-
-    <?= $form->field($model, 'status')->textInput() ?>
-
-    <?= $form->field($model, 'shiped_at')->textInput() ?>
-
-    <?= $form->field($model, 'delivered_at')->textInput() ?>
-
-    <?= $form->field($model, 'tracking_number')->textInput(['maxlength' => true]) ?>
-
-    <div class="form-group">
+    <div class="col-sm-6 form-group">
+        <?= $form->field($model, 'product_id')->textInput() ?>
+    </div>
+    <div class="col-sm-6 form-group">
+        <?= $form->field($model, 'amount')->textInput() ?>
+    </div>
+    <div class="col-sm-6 form-group">
+        <?= $form->field($model, 'payment')->textInput(['maxlength' => true]) ?>
+    </div>
+    <div class="col-sm-6 form-group">
+        <?= $form->field($model, 'user_id')->textInput() ?>
+    </div>
+    <div class="col-sm-6 form-group">
+        <?= $form->field($model, 'user_name')->textInput(['maxlength' => true]) ?>
+    </div>
+    <div class="col-sm-6 form-group">
+        <?= $form->field($model, 'created_at')->textInput() ?>
+    </div>
+    <div class="col-sm-6 form-group">
+        <?= $form->field($model, 'updated_at')->textInput() ?>
+    </div>
+    <div class="col-sm-6 form-group">
+        <?= $form->field($model, 'status')->textInput() ?>
+    </div>
+    <div class="col-sm-6 form-group">
+        <?= $form->field($model, 'shiped_at')->textInput() ?>
+    </div>
+    <div class="col-sm-6 form-group">
+        <?= $form->field($model, 'delivered_at')->textInput() ?>
+    </div>
+    <div class="col-sm-6 form-group">
+        <?= $form->field($model, 'tracking_number')->textInput(['maxlength' => true]) ?>
+    </div>
+    <div class="col-sm-12 form-group">
         <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
     </div>
 
