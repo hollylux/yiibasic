8f2560e217e91b886499ccb012aff2ba187e7502
hollylux <hollyluxl@gmail.com>
2017-05-05 17:11:20 -0400

    Tuning homepage layout..

--- views/site/index.php
+++ views/site/index.php
@@ -1,38 +1,76 @@
 <?php
-
 /* @var $this yii\web\View */
 
-$this->title = '北美40 - 一个只提供美国原装进口的站';
+$this->title = '北美40 - 从此， 开启半个美式生活';
 ?>
 <div class="site-index">
 
     <div class="jumbotron">
-        <h1>一个只有美国原装进口的站</h1>
-
-        <p class="lead">惊喜， 就是把美国Costco, Target, Walmart, Kohls, Amazon送到你指尖 </p>
-
-        <!--<p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">开抢</a></p>-->
+        <h1>北美40：从此， 开启半个美式生活</h1>
+        <p class="lead">乐享美国高级连锁Costco, Target, Walmart, Amazon专供， 包邮包税</p>
     </div>
-
     <div class="body-content">
+        <div class="row">
+             <div class="col-sm-4 col-md-3">
+                <div class="thumbnail">
+                    <img src="./mstore/170504152317GerberPuffs.jpg" alt="Gerber嘉宝星星泡芙puffs， 42g， 宝宝辅食">
+                    <div class="caption">
+                        <h5>美国Gerber嘉宝星星泡芙puffs 42g 宝宝辅食</h5>
+                        <p>¥ 38.00</p>
+                        <p><a href="#" class="btn btn-danger" role="button">喜欢</a> <a href="#" class="btn btn-success" role="button">带走</a></p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-3">
+                <div class="thumbnail">
+                    <img src="./mstore/170504153918AveenoBabySoothing.jpg" alt="美国Aveeno Baby艾维诺宝宝燕麦润肤舒缓乳液防湿疹 深蓝盖 139ml">
+                    <div class="caption">
+                        <h5>美国Aveeno Baby艾维诺宝宝燕麦润肤舒缓乳液防湿疹 139ml</h5>
+                        <p>...</p>
+                        <p><a href="#" class="btn btn-danger" role="button">喜欢</a> <a href="#" class="btn btn-success" role="button">带走</a></p>
+                    </div>
+                </div>
+            </div>
+           
+            <div class="col-sm-6 col-md-3">
+                <div class="thumbnail">
+                    <img src="..." alt="...">
+                    <div class="caption">
+                        <h5>Ayer艾儿钙C-20美国原装进口营养素饮料甘氨酸钙</h5>
+                        <p>...</p>
+                        <p><a href="#" class="btn btn-danger" role="button">喜欢</a> <a href="#" class="btn btn-success" role="button">带走</a></p>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="col-sm-6 col-md-3">
+                <div class="thumbnail">
+                    <img src="..." alt="...">
+                    <div class="caption">
+                        <h5>美国Culturelle益生菌 康萃乐LGG益生菌 维护肠胃健康 儿童咀嚼片 30粒/盒</h5>
+                        <p>...</p>
+                        <p><a href="#" class="btn btn-danger" role="button">喜欢</a> <a href="#" class="btn btn-success" role="button">带走</a></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
 
         <div class="row">
-            <div class="col-lg-4">
+            <div class="col-md-3">
                 <h2>商品</h2>
-
                 <p><a class="btn btn-default" href="./index.php?r=product">商品列表 &raquo;</a></p>
-                <p><a class="btn btn-default" href="./index.php?r=product/create">添加商品 &raquo;</a></p>
-                
             </div>
-            <div class="col-lg-4">
+            <div class="col-md-3">
                 <h2>订单</h2>
-
                 <p><a class="btn btn-default" href="">订单查询 &raquo;</a></p>
             </div>
-            <div class="col-lg-4">
+            <div class="col-md-3">
+                <h2>活动</h2>
+                <p><a class="btn btn-default" href=""> 数据统计 &raquo;</a></p>
+            </div>
+             <div class="col-md-3">
                 <h2>统计</h2>
-
-
                 <p><a class="btn btn-default" href=""> 数据统计 &raquo;</a></p>
             </div>
         </div>
